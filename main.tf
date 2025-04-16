8109b18f3de40a977b4291767f760fe763105530
Nastia <63830027+its-nastia@users.noreply.github.com>
2025-04-16 11:52:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web_server" {
               #!/bin/bash
               yum update -y
               PUBLIC_IP=$(curl -s https://api.ipify.org)
-              curl -X GET "https://devops:11653bd900a61a61152c8caab3b8f24def@jenkins-ops.portnov.com/job/nastia-webhooks/buildWithParameters?token=Abc123456&TEST_PARAM=$PUBLIC_IP"
+              curl -X GET "https://devops:11653bd900a61a61152c8caab3b8f24def@jenkins-ops.portnov.com/job/nastia-tf-task/buildWithParameters?token=Abc123456&MANAGED_HOST=$PUBLIC_IP"
               EOF
 }
 
